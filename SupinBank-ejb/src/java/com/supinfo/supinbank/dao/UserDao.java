61054a820653dfaffc419a253e14efe27e19b6df
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-02-09 17:53:32 +0100

    Add User DAO with JPA.

---
+++ SupinBank-ejb/src/java/com/supinfo/supinbank/dao/UserDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.supinfo.supinbank.dao;
+
+import com.supinfo.supinbank.entity.User;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author nico
+ */
+@Local
+public interface UserDao 
+{
+    List<User> getAllCustomers();
+    
+    User authenticateUserWithIdPassword(Long userId, String clearPassword);
+    
+    void addUser(User u);
+}
---
+++ SupinBank-ejb/src/java/com/supinfo/supinbank/dao/jpa/JpaUserDao.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.supinfo.supinbank.dao.jpa;
+
+import com.supinfo.supinbank.dao.UserDao;
+import com.supinfo.supinbank.entity.User;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author nico
+ */
+@Stateless
+public class JpaUserDao implements UserDao
+{
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Override
+    public List<User> getAllCustomers() 
+    {
+        return em.createQuery("SELECT c FROM Customer c WHERE c.userType = 'Customer'").getResultList();
+    }
+
+    @Override
+    public User authenticateUserWithIdPassword(Long userId, String clearPassword) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void addUser(User u) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
